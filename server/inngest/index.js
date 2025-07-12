a27960bbf92f16c0fb90fbd3fd45c2170afa0d33
S-Git-Coder <sahilpatel9313@gmail.com>
2025-07-12 15:48:50 +0530

    Function export fix

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -40,4 +40,4 @@ const syncUserUpdation = inngest.createFunction(
     }
 )
 
-export const functions = [syncUserCreation, syncUserDeletion];
+export const functions = [syncUserCreation, syncUserDeletion, syncUserUpdation];
